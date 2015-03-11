f1ce5aa49301668ad6e66a0a17298d3eb385d91a
Matthieu Rosinski <korrigan@slash16.org>
2015-03-11 18:26:38 +0100

    handling 404 cases in proxy update

--- hapapi/api/proxy.py
+++ hapapi/api/proxy.py
@@ -2,11 +2,13 @@
 HAproxy views representing a proxy (frontend)
 
 """
+from flask import abort
 from flask.views import MethodView
 from flask.json import jsonify
 
 from .mixins import HAProxyViewMixin
 from .auth import authentication_required
+
 
 class ProxyView(MethodView, HAProxyViewMixin):
     """
@@ -22,12 +24,17 @@ class ProxyView(MethodView, HAProxyViewMixin):
     def get(self, name=None):
         if not name:
             return self.list()
+        try:
+            proxy = self.haproxy.get_proxy(name)
+        except:
+            abort(404)
+        proxy.update({
+            'backends': list(proxy['backends'].keys()),
+            'name': name,
+        })
         return jsonify({
-            'proxy': {
-                'name': name,
-                'backends': self.haproxy.list_backends(name),
-                }
-            })
+            'proxy': proxy,
+        })
 
     @classmethod
     def register(klass, app):
