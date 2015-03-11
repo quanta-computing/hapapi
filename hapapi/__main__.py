49b62f4ef5e2fddcd2d637ffffe37148f10b4ade
Matthieu Rosinski <korrigan@slash16.org>
2015-03-11 18:27:15 +0100

    added options to hapapi

--- hapapi/__main__.py
+++ hapapi/__main__.py
@@ -8,8 +8,23 @@ def main():
     Main function for HApAPI
 
     """
+    import argparse
     from hapapi.api import setup
-    setup().run(debug=True)
+    parser = argparse.ArgumentParser(
+        description='HApAPI is a REST API to interact with HAproxy unix socket interface',
+        epilog='Copyright Quanta 2015',
+    )
+    parser.add_argument('-b', '--bind', default='127.0.0.1',
+        help='On wich ip address we listen for incoming connections'
+    )
+    parser.add_argument('-p', '--port', type=int, default=5000,
+        help='On wich port we listen for incoming connections'
+    )
+    parser.add_argument('-f', '--config', default='/etc/haproxy/hapapi.cfg',
+        help='Path to a config file'
+    )
+    opts = parser.parse_args()
+    setup(opts.config).run(opts.bind, opts.port)
 
 
 if __name__ == "__main__":
